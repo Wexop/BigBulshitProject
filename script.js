3b65f7b5b56cf6594691f0e951ec20ce1585fbd5
Wexop <mattheurtin@gmail.com>
2022-06-21 21:40:37 +0200

    delete too long word

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let color = "red";
 let colorTab = ["red", "white", "blue", "green", "yellow", "#00ff7b", "#4c00ff", "#ff00bf", "#7bff00", "#ff7300"];
 let colorTabRevolt = [];
 let wordsTab = ["Enculer", "Connard", "grosse pute", "je t'encule", "gros con", "ta gueule",
-    "batard", "sale truelle à sperme", "espèce d'andouille", "Orchidoclaste", "enfoiré"];
+    "batard", "sale truelle à sperme", "espèce d'andouille", "enfoiré"];
 const wordOfThisPage = wordsTab[Math.random() * wordsTab.length | 0];
 
 document.getElementById('BigTexte').textContent = wordOfThisPage
