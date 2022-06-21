90bab7810e465e61dd6708c864494e3fc182b694
Wexop <mattheurtin@gmail.com>
2022-06-21 21:08:50 +0200

    Add color change + random words

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-document.getElementById("BigTexte").style.borderColor = "red";
-alert("OUI");
+let color = "red";
+let colorTab = ["red", "white", "blue", "green", "yellow"];
+let colorTabRevolt = [];
+let wordsTab = ["Enculer", "Connard", "grosse pute", "fils de pute", "je t'emmerde", "gros con", "salope"];
+const wordOfThisPage = wordsTab[Math.random() * wordsTab.length | 0];
+
+document.getElementById('BigTexte').textContent = wordOfThisPage
+
+function randColor(array) {
+    var rand = Math.random() * array.length | 0;
+    var rValue = array[rand]
+
+    return rValue;
+}
+
+function changeColor() {
+    colorTabRevolt = colorTab.filter(function (f) {
+        return f !== color
+    });
+    color = randColor(colorTabRevolt)
+    document.getElementById('BigTexte').style.color = color;
+}
+
+setInterval(changeColor, 200);
