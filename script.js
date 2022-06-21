21653881ad12c836c977fbbdecfef924066c29c4
Wexop <mattheurtin@gmail.com>
2022-06-21 23:50:00 +0200

    New structure

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
 let color = "red";
 let colorTab = ["red", "white", "blue", "green", "yellow", "#00ff7b", "#4c00ff", "#ff00bf", "#7bff00", "#ff7300"];
 let colorTabRevolt = [];
-let wordsTab = ["Enculer", "Connard", "grosse pute", "je t'encule", "gros con", "ta gueule",
+let wordsTab = ["Enculé", "Connard", "je t'encule", "gros con", "ta gueule",
     "batard", "sale truelle à sperme", "espèce d'andouille", "enfoiré"];
-const wordOfThisPage = wordsTab[Math.random() * wordsTab.length | 0];
+const wordOfThisPage = decodeURI(getInLink("word"));
+let focus = "BigTexte"
+if (wordOfThisPage !== "null") {
+    document.getElementById('BigTexte').textContent = wordOfThisPage
+} else {
+    focus = "homeButton"
+    document.getElementById('BigTexte').style.textDecoration = "none";
+    document.getElementById('BigTexte').innerHTML = '<a href = "index.html" id="homeButton">' + " Va te créer un lien bg" + '</a>';
+}
 
-document.getElementById('BigTexte').textContent = wordOfThisPage
+function getVal() {
+    const val = document.getElementById("word").value;
+    document.getElementById("buttonInHome").href = "permaWord.html?word=" + val;
+    document.getElementById("word").action = "permaWord.html?word=" + val;
+}
+
+function getInLink(param) {
+    var vars = {};
+    window.location.href.replace(location.hash, '').replace(
+        /[?&]+([^=&]+)=?([^&]*)?/gi, // regexp
+        function (m, key, value) { // callback
+            vars[key] = value !== undefined ? value : '';
+        }
+    );
+
+    if (param) {
+        return vars[param] ? vars[param] : null;
+    }
+    return vars;
+}
+
 
 function randColor(array) {
     var rand = Math.random() * array.length | 0;
@@ -19,7 +47,7 @@ function changeColor() {
         return f !== color
     });
     color = randColor(colorTabRevolt)
-    document.getElementById('BigTexte').style.color = color;
+    document.getElementById(focus).style.color = color;
 }
 
 setInterval(changeColor, 200);
