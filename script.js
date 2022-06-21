6b8e501992b36e686c3ddc3fc149753ced4d4870
Wexop <mattheurtin@gmail.com>
2022-06-21 21:39:38 +0200

    add words

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let color = "red";
 let colorTab = ["red", "white", "blue", "green", "yellow", "#00ff7b", "#4c00ff", "#ff00bf", "#7bff00", "#ff7300"];
 let colorTabRevolt = [];
-let wordsTab = ["Enculer", "Connard", "grosse pute", "fils de pute", "je t'encule", "gros con", "salope", "ta gueule",
-    "batard", "sale truelle à sperme", "fils de chienne", "nique ta race"];
+let wordsTab = ["Enculer", "Connard", "grosse pute", "je t'encule", "gros con", "ta gueule",
+    "batard", "sale truelle à sperme", "espèce d'andouille", "Orchidoclaste", "enfoiré"];
 const wordOfThisPage = wordsTab[Math.random() * wordsTab.length | 0];
 
 document.getElementById('BigTexte').textContent = wordOfThisPage
