b7565fd2179e93d8492fc5dce07547cfe033b260
Wexop <mattheurtin@gmail.com>
2022-06-22 20:43:24 +0200

    Chiffrement de l'url pour pas deviner le mot

--- script.js
+++ script.js
@@ -1,22 +1,76 @@
 let color = "red";
 let colorTab = ["red", "white", "blue", "green", "yellow", "#00ff7b", "#4c00ff", "#ff00bf", "#7bff00", "#ff7300"];
 let colorTabRevolt = [];
+
+const crypteDico = {
+    "a": "ç", "b": "<3", "c": "$", "d": "x", "e": ",", "f": "é", "g": "*", "h": ":)", "i": "v",
+    "j": "=", "k": ":", "l": "jsp", "m": "€", "n": "7", "o": "p", "p": "o", "q": "@", "r": "q",
+    "s": "à", "t": "(", "u": "n", "v": ".", "w": ";", "x": "^^", "y": ";(", "z": ":D",
+    "A": "#<!>", "B": "<!>#", "C": "<!!>", "D": "<££>", "E": "<$$>", "F": "<//>", "G": "[##]",
+    "H": "<;:>", "I": "<><", "J": "><>", "K": "<..>", "L": ";:;;", "M": ":;:;", "N": "->->",
+    "O": "<-->", "P": "<<", "Q": ">>", "R": "--", "S": "__", "T": "!*", "U": "*!", "V": "?;",
+    "W": "{]", "X": "%!", "Y": "§§", "Z": "_:_", "0": "+++", "1": "kr", "2": "ôp", "3": ":p",
+    "4": "~~", "5": "/,", "6": "°}", "7": "|_|", "8": "|-|", "9": "_-_", ",": "LOL", ".": "???",
+    ":": "èé", "?": "£$", "-": "°_°", "(": "WW", ")": "VVVV", "é": "456", "è": "001",
+    "ç": "oula", "à": "%+", "ù": "oui", " ": "69", "<": "µ", ">": "£$€", "ê": "bob", "'": "::::",
+    "ô": "[[", "î": ")]", "!": ":/", "[": "++", "]": "ùû", "û": "{:)", "€": "=$=", "$": "=€=",
+    "ö": "idk", "ë": "zzz", '"': "!?./"
+}
+
+
 let wordsTab = ["Enculé", "Connard", "je t'encule", "gros con", "ta gueule",
     "batard", "sale truelle à sperme", "espèce d'andouille", "enfoiré"];
-const wordOfThisPage = decodeURI(getInLink("word"));
+
+const wordOfThisPage = decodeURI(getInLink("word")).replaceAll("+", " ");
+const wordDechiffre = dechifrement(wordOfThisPage, crypteDico);
 let focus = "BigTexte"
 if (wordOfThisPage !== "null") {
-    document.getElementById('BigTexte').textContent = wordOfThisPage.replace("+", " ")
+    document.getElementById('BigTexte').textContent = wordDechiffre
 } else {
     focus = "homeButton"
     document.getElementById('BigTexte').style.textDecoration = "none";
     document.getElementById('BigTexte').innerHTML = '<a href = "index.html" id="homeButton">' + " Va te créer un lien bg" + '</a>';
 }
 
+
+function chiffrement(mot, dic) {
+    let texteChiffre = "";
+    let splitMot = mot.split(" ");
+    for (let i = 0; i < splitMot.length; i++) {
+        for (let y = 0; y < splitMot[i].length; y++) {
+            if (splitMot[i][y] in dic) {
+                texteChiffre += dic[splitMot[i][y]] + " ";
+            } else {
+                texteChiffre += splitMot[i][y] + " ";
+            }
+
+        }
+        texteChiffre += dic[" "] + " ";
+    }
+    return texteChiffre
+
+}
+
+function dechifrement(mot, dic) {
+    let texteDechiffre = "";
+    let motSplit = mot.split(" ");
+    for (let i = 0; i < motSplit.length; i++) {
+        for (let key in dic) {
+            if (dic[key] === motSplit[i]) {
+                texteDechiffre += key;
+            }
+
+
+        }
+    }
+    return texteDechiffre
+}
+
 function getVal() {
-    const val = document.getElementById("word").value;
-    document.getElementById("buttonInHome").href = "permaWord.html?word=" + val;
-    document.getElementById("word").action = "permaWord.html?word=" + val;
+    let val = document.getElementById("word").value;
+    let valChiffree = chiffrement(val, crypteDico);
+    document.getElementById("buttonInHome").href = "permaWord.html?word=" + valChiffree;
+    document.wordForm.action = "permaWord.html?word=" + valChiffree;
 }
 
 function getInLink(param) {
